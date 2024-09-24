718b8a92581bf3cebfe19d4c3e9464d044478120
роман хожанец <rkhozhanets.1910@mail.ru>
2024-09-24 15:15:26 +0700

    Добавьте файлы проекта.

---
+++ Lab2_AIS_Server/CarAuction.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1AIS
+{
+    public class CarAuction
+    {
+        public string Maker { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public string VIN { get; set; }
+
+        public int? Price { get; set; }
+
+        public int? YearProd { get; set; }
+
+        public int? Mileage { get; set;}
+
+        public bool? IsSold { get; set;}
+
+        public bool? HasAccidents { get; set;}
+
+        public CarAuction(string maker, string model, string color, string vin, int price, int yearProd, int mileage, bool issold, bool hasAccidents) 
+        {
+            Maker = maker;
+            Model = model;
+            Color = color;
+            VIN = vin;
+            Price = price;
+            YearProd = yearProd;
+            Mileage = mileage;
+            IsSold = false;
+            HasAccidents = hasAccidents;
+        }
+
+        public CarAuction()
+        {
+            Maker = "";
+            Model = "";
+            Color = "";
+            VIN = "";
+            Price = null;
+            YearProd = null;
+            Mileage = null;
+            IsSold = false;
+            HasAccidents = null;
+        }
+
+        public override string ToString()
+        {
+            return $"{Maker},{Model},{Color},{VIN},{Price},{YearProd},{Mileage},{IsSold},{HasAccidents}";
+        }
+
+        public static CarAuction Parse(string str)
+        {
+            string[] parts = str.Split(',');
+            if (parts.Length != 9) throw new FormatException("Неверный формат данных.");
+            return new CarAuction(parts[0], parts[1], parts[2], parts[3], int.Parse(parts[4]), int.Parse(parts[5]), int.Parse(parts[6]), bool.Parse(parts[7]), bool.Parse(parts[8]));
+        }
+    }
+}
---
+++ Lab2_AIS_Server/Server.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using System.IO;
+
+class Server
+{
+    private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+    private static readonly string pathFile = @"D:\text.csv"; // Добавлен относительный путь к файлу
+
+    public static async Task Main(string[] args)
+    {
+        var config = new NLog.Config.LoggingConfiguration();
+
+        // Targets where to log to: File and Console
+        var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "logs.txt" };
+        var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+        // Rules for mapping loggers to targets            
+        config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+        config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+        // Apply config           
+        NLog.LogManager.Configuration = config;
+
+        UdpClient udpClient = new UdpClient(8080);
+        logger.Info("Сервер запущен и ожидает запросов.");
+
+        try
+        {
+            while (true)
+            {
+                var receivedResult = await udpClient.ReceiveAsync();
+                string[] request = Encoding.UTF8.GetString(receivedResult.Buffer).Split(":");
+                logger.Info($"Получен запрос: {request}");
+
+                string response = ProcessRequest(request);
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+                await udpClient.SendAsync(responseBytes, responseBytes.Length, receivedResult.RemoteEndPoint);
+                logger.Info("Ответ отправлен клиенту.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    private static string ProcessRequest(string[] request)
+    {
+        if (request[0] == "get_all_data")
+        {
+            logger.Info("Запрос на получение всех данных.");
+            return GetAllData();
+        }
+        else if (request[0] == "get_str")
+        {
+            int recordId = int.Parse(request[1]);
+            logger.Info($"Запрос на получение записи с номером {recordId}.");
+            return GetRecord(recordId);
+        }
+        else if (request[0] == "del_str")
+        {
+            int recordId = int.Parse(request[1]);
+            logger.Info($"Запрос на удаление записи с номером {recordId}.");
+            return DeleteRecord(recordId);
+        }
+        else if (request[0] == "add_str")
+        {
+            string recordData = request[1];
+            logger.Info($"Запрос на добавление новой записи: {recordData}.");
+            return AddRecordToFile(recordData);
+        }
+        else
+        {
+            logger.Warn("Неизвестный запрос.");
+            return "Unknown command";
+        }
+    }
+
+    public static string DeleteRecord(int line_num)
+    {
+        List<string> lines = new List<string>();
+
+        using (StreamReader sr = new StreamReader(pathFile))
+        {
+            string str;
+            while ((str = sr.ReadLine()) != null)
+            {
+                lines.Add(str);
+            }
+        }
+
+        if (line_num <= 0 || line_num > lines.Count)
+        {
+            return "Ошибка индекса строки";
+        }
+
+        lines.RemoveAt(line_num - 1);
+
+        using (StreamWriter writer = new StreamWriter(pathFile))
+        {
+            foreach (string line in lines)
+            {
+                writer.WriteLine(line);
+            }
+        }
+        return "Строка удалена";
+    }
+
+    public static string GetAllData()
+    {
+        StringBuilder response = new StringBuilder();
+
+        using (StreamReader sr = new StreamReader(pathFile))
+        {
+            string str;
+            while ((str = sr.ReadLine()) != null)
+            {
+                response.AppendLine(str);
+            }
+        }
+        return response.ToString();
+    }
+
+    public static string GetRecord(int recordId)
+    {
+        string response = "";
+
+        using (StreamReader sr = new StreamReader(pathFile))
+        {
+            for (int i = 0; i < recordId - 1; i++)
+            {
+                sr.ReadLine();
+            }
+            response = sr.ReadLine();
+        }
+        return response ?? "Запись не найдена";
+    }
+
+    public static string AddRecordToFile(string recordData)
+    {
+        try
+        {
+            using (StreamWriter sw = new StreamWriter(pathFile, true))
+            {
+                sw.WriteLine(recordData);
+            }
+            return "Данные успешно записаны в файл.";
+        }
+        catch (Exception ex)
+        {
+            return $"Произошла ошибка при записи в файл: {ex.Message}";
+        }
+    }
+}
